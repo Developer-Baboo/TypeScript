e1e3dbdc50bb9753845dfa5554fe31890ab15e5e
Developer Baboo <babooheerani@gmail.com>
2023-10-06 11:01:34 +0500

    type annotations

---
+++ Types/type_annotations.ts
@@ -0,0 +1,69 @@
+// let first_number: number = 5;
+// let second_number: number = 6;
+
+
+// let decimal: number = 5.1;
+// let myNegativeval: number = -5;
+
+
+
+// let third_number: string = "Baboo Kumar";
+// console.log(second_number);
+
+
+/* let myFullName = "Baboo Kumar";
+let myFirstName = "Baboo";
+let myLastName = "Kumar";
+let FullName = myFirstName + myLastName;
+console.log(FullName); */
+
+// Arithematic operator
+let num1: number = 10;
+let num2: number = 5;
+
+let sum1: number = num1 + num2;
+let difference: number = num1 - num2;
+
+console.log("Sum: " + sum1); // Output: Sum: 15
+console.log("Difference: " + difference); // Output: Difference: 5
+
+
+// String Initialize
+// let message: string = "Hello, TypeScript!";
+// console.log(message); // Output: Hello, TypeScript!
+
+
+//String Concatenation:
+// let firstName: string = "John";
+// let lastName: string = "Doe";
+
+// let fullName: string = firstName + " " + lastName;
+// console.log("Full Name: " + fullName); // Output: Full Name: John Doe
+
+
+/* // String Length:
+let str: string = "Hello, World!";
+let length1: number = str.length;
+
+console.log("Length of the string: " + length1); // Output: Length of the string: 13 */
+
+
+/* // String Comparison:
+let str1: string = "apple";
+let str2: string = "orange";
+
+if (str1 === str2) {
+    console.log("Strings are equal.");
+} else {
+    console.log("Strings are not equal.");
+}
+// Output: Strings are not equal. */
+
+
+/* //String Template
+let name1: string = "Alice";
+let greeting: string = `Hello, ${name1}! Welcome to TypeScript.`;
+
+console.log(greeting);
+// Output: Hello, Alice! Welcome to TypeScript. */
+
