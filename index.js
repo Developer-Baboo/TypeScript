ddb6d5c702e477eb8f709519ba377ebab905fbe6
Developer Baboo <babooheerani@gmail.com>
2023-10-06 07:28:07 +0500

    type annotations

---
+++ index.js
@@ -0,0 +1,12 @@
+/* console.log("i am Baboo Kumar");
+let b = 5;
+b = "f"; // number not assign to type string
+// Typescript infers num as numbers */
+/* let num: number = 5;
+num = "Baboo Kumar"; //Error */
+/* let num: number = 5;
+console.log(num); */
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 10));
