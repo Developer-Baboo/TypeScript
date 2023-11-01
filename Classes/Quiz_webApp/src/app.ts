0f79c5dc2c4e2e7c3d5c4aad980f9fe660ece2af
Developer Baboo <babooheerani@gmail.com>
2023-11-01 22:22:17 +0500

    done with logged in system

--- Classes/Quiz_webApp/src/app.ts
+++ Classes/Quiz_webApp/src/app.ts
@@ -1,1 +1,33 @@
-console.log("Hello Js");
+// Define user data array
+const users: { email: string; password: string }[] = [
+  { email: 'user@example.com', password: 'password123' },
+  // Add more user data as needed
+];
+
+// Get form and error message element
+const registrationForm = document.getElementById('registrationForm') as HTMLFormElement;
+const errorMessage = document.getElementById('error-message') as HTMLDivElement;
+
+// Add form submission event listener
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get user input values
+  const emailInput = document.getElementById('email') as HTMLInputElement;
+  const passwordInput = document.getElementById('password') as HTMLInputElement;
+  const enteredEmail = emailInput.value;
+  const enteredPassword = passwordInput.value;
+
+  // Check if entered email and password match with any user data
+  const matchedUser = users.find(user => user.email === enteredEmail && user.password === enteredPassword);
+
+  if (matchedUser) {
+    // Login successful
+    errorMessage.textContent = '';
+    alert('Login successful!');
+  } else {
+    // Login failed, show error message
+    errorMessage.textContent = 'Invalid email or password. Please try again.';
+  }
+});
+
